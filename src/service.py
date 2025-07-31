03c58434a75bcd2feba45463e464518d4e088681
Blair Hoddinott <blair.hoddinott@gmail.com>
2025-07-31 01:22:46 -0400

    BH: Progressing

---
+++ src/service.py
@@ -0,0 +1,114 @@
+import argparse
+import logging
+import requests
+import structlog
+import os
+import sys
+import time
+
+from urllib3.exceptions import InsecureRequestWarning
+from dotenv import load_dotenv
+from structlog import get_logger
+from technitium import TechnitiumDNS
+
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+structlog.configure(
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso", utc=False),
+        structlog.processors.add_log_level,
+        structlog.dev.ConsoleRenderer(),
+    ]
+)
+log = get_logger()
+load_dotenv()
+
+API_URL = os.getenv("API_URL")
+API_TOKEN = os.getenv("API_TOKEN")
+REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+REDIS_KEY = os.getenv("REDIS_KEY", "dns_update")
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+REDIS_DB = os.getenv("REDIS_DB", 4)
+VALID_RECORD_TYPES = [
+    "A",
+    "AAAA",
+    "CNAME",
+    "MX",
+    "TXT"
+]
+
+
+def validate_environment():
+    if API_URL is None or API_TOKEN is None:
+        log.critical(
+            "Confirm the .env file has been populated.",
+            API_URL=API_URL,
+            API_TOKEN=API_TOKEN,
+            REDIS_HOST=REDIS_HOST,
+            REDIS_PORT=REDIS_PORT
+        )
+        sys.exit(1)
+
+
+def run():
+    validate_environment()
+
+    dns = TechnitiumDNS(API_URL, API_TOKEN, REDIS_HOST)
+    while True:
+        records = dns.get_from_redis()
+        if records:
+            log.debug("Record found in redis", records=records["records"])
+            if dns.check_for_dns_record(args.domain, "test", "TXT", None):
+                log.info("Running validation check")
+                if dns.check_for_validation_complete():
+                    log.info(
+                        "Validation complete, removing TXT record from DNS",
+                        record=records["records"]
+                    )
+                    for record in records["records"]:
+                        if dns.delete_dns_record(args.domain, record["name"], record["type"],
+                                                record["value"]):
+                            log.info("Removing record from redis")
+                            if dns.remove_records_from_redis():
+                                log.info("Records removed, validation loop completed.")
+                        else:
+                            log.warn("error removing DNS record")
+                else:
+                    log.debug("Validation in progress or otherwise not ready to clean out records")
+            else:
+                log.info("Adding record from redis", records=records["records"])
+                for record in records["records"]:
+                    dns.add_dns_record(
+                        args.domain,
+                        record["name"],
+                        record["type"],
+                        record["value"]
+                    )
+
+        time.sleep(300)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="A service that monitors for required changes to DNS"
+    )
+    parser.add_argument(
+        "-d",
+        "--domain",
+        action="store",
+        required=True,
+        help="The domain you wish to query within Namecheap"
+    )
+    parser.add_argument(
+        "-v",
+        "--debug",
+        action="store_true",
+        help="Turns on debug logging"
+    )
+    args = parser.parse_args()
+    if args.debug:
+        structlog.configure(wrapper_class=structlog.make_filtering_bound_logger(logging.DEBUG),)
+    else:
+        structlog.configure(wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),)
+
+    run()
--- src/technitium.py
+++ src/technitium.py
@@ -27,6 +27,7 @@ class TechnitiumDNS():
         self.REDIS_PORT = redis_port
         self.REDIS_KEY = redis_key
         self.REDIS_DB = redis_db
+        self.REDIS_VALIDATION_KEY = "validation_complete"
 
     def add_dns_record(self, zone, record_name, record_type, value, ttl=60):
         if value is None:
@@ -74,6 +75,40 @@ class TechnitiumDNS():
         else:
             log.info("Added record", added_record=response.json()["response"]["addedRecord"])
 
+    def check_for_dns_record(self, zone, record_name, record_type, value):
+        response = requests.get(
+            f"{self.API_URL}/zones/records/get"
+            f"?domain={record_name}.{zone}"
+            f"&zone={zone}"
+            f"&token={self.API_TOKEN}",
+            verify=False
+        )
+        try:
+            data = response.json()
+        except Exception as e:
+            log.critical("Unable to process response", exception=e, response=response)
+            return False
+
+        if data.get("status") == "ok":
+            records = data.get("response", {}).get("records", [])
+            log.debug("Raw data", records=data)
+            if not records:
+                log.debug("DNS record not found")
+                return False
+            else:
+                log.debug("", records=records)
+                for record in records:
+                    log.info("Record found", name=record["name"], type=record["type"],
+                        data=record["rData"])
+                return True
+        else:
+            log.warn(
+                "Failed to fetch records:",
+                status_code=response.status_code,
+                error_message=data["errorMessage"]
+            )
+            return False
+
     def delete_dns_record(self, zone, record_name, record_type, value):
         if record_type == "A" or record_type == "AAAA":
             payload = f"{self.API_URL}/zones/records/delete" \
@@ -104,8 +139,10 @@ class TechnitiumDNS():
         response = requests.post(payload, verify=False)
         if response.json()["status"] != "ok":
             log.info("Error deleting record:", response=response.json())
+            return False
         else:
             log.info("Removed record")
+            return True
 
     def list_zone_records(self, zone):
         response = requests.get(
@@ -146,3 +183,47 @@ class TechnitiumDNS():
             return record_dict
         else:
             log.warn("Key not found in Redis", key=self.REDIS_KEY)
+
+        return None
+
+    def check_for_validation_complete(self):
+        """
+        This function checks the redis key for validation completion. 
+        If this is set to true, it will delete the record in DNS and then unset the redis keys.
+        This is part of my split-brain DNS validation issue, not likely to be of use to others
+        """
+        r = redis.Redis(host=self.REDIS_HOST, port=self.REDIS_PORT, db=self.REDIS_DB)
+        if r.exists(self.REDIS_VALIDATION_KEY):
+            validation_complete = bool(int(r.get(self.REDIS_VALIDATION_KEY)))
+            log.debug("validation status", is_validation_complete=validation_complete)
+            if validation_complete:
+                return True
+        else:
+            log.debug("validation key is not set")
+        return False
+
+    def remove_records_from_redis(self):
+        r = redis.Redis(host=self.REDIS_HOST, port=self.REDIS_PORT, db=self.REDIS_DB)
+        if r.exists(self.REDIS_KEY):
+            try:
+                r.delete(self.REDIS_KEY)
+            except Exception as e:
+                log.critical("Error removing redis key", exception=e, key=self.REDIS_KEY)
+                return False
+        else:
+            log.debug("Redis key is not set", key=self.REDIS_KEY)
+
+        if r.exists(self.REDIS_VALIDATION_KEY):
+            try:
+                r.delete(self.REDIS_VALIDATION_KEY)
+            except Exception as e:
+                log.critical(
+                    "Error removing redis key",
+                    excpetion=e,
+                    key=self.REDIS_VALIDATION_KEY
+                )
+                return False
+        else:
+            log.debug("Redis key is not set", key=self.REDIS_VALIDATION_KEY)
+
+        return True
